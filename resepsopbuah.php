2844b4d2d45213d46f8cf724afef99c4ed878d34
shelliawati <ellashellia1326@gmail.com>
2024-12-05 13:59:50 +0700

    menambahkan resep sop buah

--- resepsopbuah.php
+++ resepsopbuah.php
@@ -1,4 +1,5 @@
 <?php
+//apip
 echo "Bahan-bahan : ";
 echo "--------------";
 echo "1. 1/4 buah semangka, bentuk bulat";
@@ -8,3 +9,12 @@ echo "4. 10 buah stroberi, iris-iris";
 echo "5. 5 biji buah nangka, potong kotak";
 echo "6. 1/2 buah nanas";
 echo "7. 1 kaleng leci";
+
+//ella
+echo "8. 2 sdm biji selasih";
+echo "9. 1 liter susu cair full cream";
+echo "10. 200 ml kental manis";
+echo "11. 100 ml air gula";
+echo "12. 1/2 buah nanas";
+echo "11. 1 sdt perisa leci";
+echo "es batu secukupnya";
