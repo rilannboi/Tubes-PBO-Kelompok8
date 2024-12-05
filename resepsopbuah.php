ce534c13979144297d5aff1312efdccb090a7d67
rilannboi <mohammadafiffahrilan@gmail.com>
2024-12-05 13:39:27 +0700

    inisiasi resep membuat sop buah

---
+++ resepsopbuah.php
@@ -0,0 +1,10 @@
+<?php
+echo "Bahan-bahan : ";
+echo "--------------";
+echo "1. 1/4 buah semangka, bentuk bulat";
+echo "2. 1/3 buah melon, bentuk bulat";
+echo "3. 1 buah apel, potong kotak";
+echo "4. 10 buah stroberi, iris-iris";
+echo "5. 5 biji buah nangka, potong kotak";
+echo "6. 1/2 buah nanas";
+echo "7. 1 kaleng leci";
